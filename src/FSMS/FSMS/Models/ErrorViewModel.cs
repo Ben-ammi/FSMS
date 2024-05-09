dd7c19c77a0a271ff395d11bcc678439a9327698
hei karata <herishine00@gmail.com>
2024-05-09 20:21:03 +0300

    Project Initiation

---
+++ src/FSMS/FSMS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FSMS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

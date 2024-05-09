3bde9a2ec3fc7755249054faf85b7d132fa687e8
hei karata <herishine00@gmail.com>
2024-05-09 20:58:08 +0300

    Added AuthenticationController and updated document layout
    Added several documents to the "Documents" array in `DocumentLayout.json` and updated `FSMS.csproj` with a new package reference. Added scaffolding properties to `FSMS.csproj.user` and changed the default routing pattern in `Program.cs`. Added a new navigation item in `_Layout.cshtml` for the `Authentication` controller's `Index` action. Created `AuthenticationController.cs` with an `Index` action and added `Index.cshtml` as a basic HTML document.

---
+++ src/FSMS/FSMS/Controllers/AuthenticationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FSMS.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

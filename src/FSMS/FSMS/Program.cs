70bfc897a8b162491bc79d555e41e21da70b3fa8
hei karata <herishine00@gmail.com>
2024-05-09 21:25:58 +0300

    Added User and UserRole entities and updated DB context
    Updated `DocumentLayout.json` to include new documents related to the `User`, `UserRole`, `AppDbContext`, and `InitialMigration` classes. Modified `AppDbContext.cs` to include `DbSet` properties for `User` and `UserRole` entities. Removed `Entities` folder from `FSMS.csproj`. Changed the database connection string in `Program.cs` from `DefaultConnection` to `AppDbConnection`. Added `User.cs` and `UserRole.cs` files to define the new entities. Included initial database migration in `20240509182417_InitialMigration.cs` to create `UserRoles` and `Users` tables. Added `20240509182417_InitialMigration.Designer.cs` and `AppDbContextModelSnapshot.cs` for EF Core metadata.

--- src/FSMS/FSMS/Program.cs
+++ src/FSMS/FSMS/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<AppDbContext>(
-    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("AppDbConnection"))
     );
 
 var app = builder.Build();

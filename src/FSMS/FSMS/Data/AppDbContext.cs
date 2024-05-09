37ddbf9105b74519022f09aeef0e8b89deb484b3
hei karata <herishine00@gmail.com>
2024-05-09 21:01:41 +0300

    Added DbContext and installed EF Core package

---
+++ src/FSMS/FSMS/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FSMS.Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}

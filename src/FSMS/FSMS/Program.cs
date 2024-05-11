35ee8bac3a4c70bb1c41ab0131e9bd0dc45aa16b
hei karata <herishine00@gmail.com>
2024-05-12 00:49:33 +0300

    Refactored code and updated UI layout
    Modified `DocumentLayout.json` to remove certain references and changed selected child index. Added `Api` folder to `FSMS.csproj` and updated scaffolder ID and category path in `FSMS.csproj.user`. Changed routing pattern in `Program.cs`. Updated `_Layout.cshtml` with new stylesheet link and replaced sections with `@RenderBody()`. Cleared `_Layout.cshtml.css` of all styles and updated body styles in `site.css`. Cleared `site.js` of all JavaScript code. Removed `_Menu.cshtml` and `_Menu.cshtml.css` files.

--- src/FSMS/FSMS/Program.cs
+++ src/FSMS/FSMS/Program.cs
@@ -46,6 +46,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Authentication}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();

bc4443dd3d5984219c6471f996f61ef56aef9e0e
hei karata <herishine00@gmail.com>
2024-05-14 14:52:28 +0300

    Updated project structure and migrated to .NET SDK-style project
    Updated the `DocumentLayout.json` and `FSMS.sln` files to reflect changes in the project structure and a migration from a legacy C# project to a .NET SDK-style project. The paths for the `Index.cshtml`, `_Layout.cshtml.css`, and `_Layout.cshtml` files have been updated to point to the new `Users` directory.
    
    Removed some styles related to the `#content` selector from `_Layout.cshtml.css` and added a new `.DarkOrangeShade` class to `site.css`.
    
    Added a new `UsersController.cs` file with an `Index` action method, a `Index.cshtml` file for the `Index` view of the `Users` controller, and a `Index.cshtml.css` file for the styles of the `Index` view.

---
+++ src/FSMS/FSMS/Controllers/UsersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FSMS.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

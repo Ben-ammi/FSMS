5eab8996d932489dbc0a51df308999534a001329
hei karata <herishine00@gmail.com>
2024-05-31 14:19:54 +0300

    Greater Commit

---
+++ src/FSMS/FSMS/Controllers/TanksController.cs
@@ -0,0 +1,153 @@
+﻿using FSMS.Data;
+using FSMS.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FSMS.Controllers
+{
+    public class TanksController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public TanksController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Tanks
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Tanks.ToListAsync());
+        }
+
+        // GET: Tanks/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tank = await _context.Tanks
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (tank == null)
+            {
+                return NotFound();
+            }
+
+            return View(tank);
+        }
+
+        // GET: Tanks/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Tanks/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Fuel,Status,Sensors")] Tank tank)
+        {
+            if (ModelState.IsValid)
+            {
+                tank.Id = Guid.NewGuid();
+                _context.Add(tank);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tank);
+        }
+
+        // GET: Tanks/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tank = await _context.Tanks.FindAsync(id);
+            if (tank == null)
+            {
+                return NotFound();
+            }
+            return View(tank);
+        }
+
+        // POST: Tanks/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,Fuel,Status,Sensors")] Tank tank)
+        {
+            if (id != tank.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(tank);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TankExists(tank.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tank);
+        }
+
+        // GET: Tanks/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tank = await _context.Tanks
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (tank == null)
+            {
+                return NotFound();
+            }
+
+            return View(tank);
+        }
+
+        // POST: Tanks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var tank = await _context.Tanks.FindAsync(id);
+            if (tank != null)
+            {
+                _context.Tanks.Remove(tank);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TankExists(Guid id)
+        {
+            return _context.Tanks.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ src/FSMS/FSMS/Controllers/UsersController.cs
@@ -0,0 +1,161 @@
+﻿using FSMS.Data;
+using FSMS.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace FSMS.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public UsersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Users
+        public async Task<IActionResult> Index()
+        {
+            var appDbContext = _context.Users.Include(u => u.Role);
+            return View(await appDbContext.ToListAsync());
+        }
+
+        // GET: Users/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users
+                .Include(u => u.Role)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // GET: Users/Create
+        public IActionResult Create()
+        {
+            ViewData["RoleId"] = new SelectList(_context.UserRoles, "Id", "Id");
+            return View();
+        }
+
+        // POST: Users/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Username,Password,Email,FirstName,LastName,Phone,Address,City,IsActive,IsDeleted,CreatedAt,UpdatedAt,RoleId")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                user.Id = Guid.NewGuid();
+                _context.Add(user);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["RoleId"] = new SelectList(_context.UserRoles, "Id", "Id", user.RoleId);
+            return View(user);
+        }
+
+        // GET: Users/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            ViewData["RoleId"] = new SelectList(_context.UserRoles, "Id", "Id", user.RoleId);
+            return View(user);
+        }
+
+        // POST: Users/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,Username,Password,Email,FirstName,LastName,Phone,Address,City,IsActive,IsDeleted,CreatedAt,UpdatedAt,RoleId")] User user)
+        {
+            if (id != user.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(user);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserExists(user.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["RoleId"] = new SelectList(_context.UserRoles, "Id", "Id", user.RoleId);
+            return View(user);
+        }
+
+        // GET: Users/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users
+                .Include(u => u.Role)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user != null)
+            {
+                _context.Users.Remove(user);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UserExists(Guid id)
+        {
+            return _context.Users.Any(e => e.Id == id);
+        }
+    }
+}
